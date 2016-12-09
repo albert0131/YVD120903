47620562859f28618c7b4ff4123d0660ac8d41e5
albert <alber@gmail.com>
2016-12-09 11:18:27 +0800

    fragment_a add findViewById

--- app/src/main/java/com/example/user/yvd120903/Fragment_a.java
+++ app/src/main/java/com/example/user/yvd120903/Fragment_a.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +25,17 @@ public class Fragment_a extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        final TextView tv = (TextView) v.findViewById(R.id.textView);
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello Hello");
+            }
+        });
+
+        return v;
     }
 
 }
